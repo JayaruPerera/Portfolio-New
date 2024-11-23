38710761857f9ef9b7e1649fe3b6990794d01430
JayaruPerera <jayaruperera1998@gmail.com>
2024-11-23 23:53:37 +0530

    start coding hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => {
-  return <div className="flex justify-center item-center relative top-3 ">
+  return <div className="flex justify-center item-center fixed top-3 w-full">
     <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10">
       <a href="#" className="nav-item">Home</a>
       <a href="#" className="nav-item">Project</a>
