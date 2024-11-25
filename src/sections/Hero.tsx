7dde46f835d26cccd821300c64621fd714d155ad
JayaruPerera <jayaruperera1998@gmail.com>
2024-11-25 22:51:23 +0530

    adjusting the hight of CV button

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,15 +18,15 @@ export const HeroSection = () => {
         <p className="mt-4 text-center text-white/60">
         I am a software engineering student with practical experience in full-stack development and cloud computing. Proficient in frameworks like React, Next.js, and Firebase, I enjoy building scalable, user-friendly applications. As an AI enthusiast, I am passionate about integrating innovative technologies to solve real-world challenges while continuously improving my skills. I am currently seeking an internship to gain hands-on experience, contribute fresh ideas, and collaborate with dynamic teams on impactful projects.
         </p>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
-            <span className="font-semibold mt-8">Explore My Work</span>
+            <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4"/>
           </button>
-          <button inline-flex items-center gap-2 border-white bg-white text-gray-900>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
             <span>📄</span>
-            <span className="font-semibold">Download CV</span>
-          </button>
+            <span className="font-semibold ">Download CV</span>
+          </button> 
         </div>
       </div>;
   </div>
