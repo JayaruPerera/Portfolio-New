0da42b5429ec90d3224e7bb3262f3a152108b6d0
JayaruPerera <jayaruperera1998@gmail.com>
2024-11-26 08:25:30 +0530

    adjusting the content to difference screen sizes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg';
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60">
       <div className="container">
         <div className="flex flex-col items-center">
           <Image src={imageJayaru} className="size-[300px]" alt="Jayaru Perera" /> 
@@ -13,12 +13,14 @@ export const HeroSection = () => {
             <div className="text-sm font-medium">Available for work</div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">I am Jayaru Perera,</h1>
-        <h1 className="font-serif text-3xl text-center ">a Full-Stack Developer</h1>
-        <p className="mt-4 text-center text-white/60">
+        <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">I am Jayaru Perera,</h1>
+        <h1 className="font-serif text-3xl md:text-5xl text-center ">a Full-Stack Developer</h1>
+        <p className="mt-4 md:text-lg text-center text-white/60">
         I am a software engineering student with practical experience in full-stack development and cloud computing. Proficient in frameworks like React, Next.js, and Firebase, I enjoy building scalable, user-friendly applications. As an AI enthusiast, I am passionate about integrating innovative technologies to solve real-world challenges while continuously improving my skills. I am currently seeking an internship to gain hands-on experience, contribute fresh ideas, and collaborate with dynamic teams on impactful projects.
         </p>
-        <div className="flex flex-col items-center mt-8 gap-4">
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4"/>
